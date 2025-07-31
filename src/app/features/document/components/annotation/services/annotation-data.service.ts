2cc79767992ad3deca0f6db7d2f98c4b0f579c52
Aleksei Ganzha <ayganzha@mts.ru>
2025-07-31 14:33:56 +0500

    update

--- src/app/features/document/components/annotation/services/annotation-data.service.ts
+++ src/app/features/document/components/annotation/services/annotation-data.service.ts
@@ -1,20 +1,20 @@
-import { effect, inject, Injectable, signal, untracked } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { effect, inject, Injectable, signal, untracked } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
-import type { LayoutCoordinates } from '@/shared/types';
+import type { LayoutCoordinates } from "@/shared/types";
 
-import { DocumentZoomService } from '../../../services';
+import { DocumentZoomService } from "../../../services";
 
 @Injectable()
 export class AnnotationDataService {
   private readonly zoomService = inject(DocumentZoomService);
 
-  public readonly textFormControl = new FormControl('', { nonNullable: true });
+  public readonly textFormControl = new FormControl("", { nonNullable: true });
   public readonly coords = signal<LayoutCoordinates>({
     top: 0,
     left: 0,
   });
-  public readonly coordsZoom = signal(this.zoomService.zoom());
+  public readonly coordsZoom = signal(0);
 
   constructor() {
     effect(() => {
